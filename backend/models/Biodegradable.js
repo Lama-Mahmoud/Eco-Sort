7f50f0dec12bdc17186968b892a3c44b344a7ec4
lama mahmoud <lamamahmoud213@gmail.com>
2022-07-23 00:38:40 +0300

    Setting Biodegradable Schema //backend

---
+++ backend/models/Biodegradable.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+
+const biodegradableSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    amount:{
+        type:Number,
+        required:true
+    },
+    handled:{
+        type:Boolean,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    region:{
+        type:String,
+        required:true
+    },
+    city:{
+        type:String,
+        required:true
+    },
+    user_Id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required:true
+    },
+    beneficiary_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+
+});
+
+
+module.exports = mongoose.model('Biodegradable', biodegradableSchema);
