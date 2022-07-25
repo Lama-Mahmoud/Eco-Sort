1540c46b95054c81c9fc43d991d1210843e8370d
lama mahmoud <lamamahmoud213@gmail.com>
2022-07-25 18:18:37 +0300

    Frontend:Adding MainPage class

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(
+  const MaterialApp(home: MainPage(),
 ));
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor:Color(0xFF5BB259),
+      body:Center(
+        child: Image(
+          image: AssetImage("assets/White_Logo.png")
+          ),
+      ),
+    );
+  }
+}
