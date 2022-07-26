077d7db6a94d2907751ceab337324abd49fd4439
lama mahmoud <lamamahmoud213@gmail.com>
2022-07-27 02:40:45 +0300

    Frontend: login Form finaliztaion

--- frontend/lib/Users/user/login.dart
+++ frontend/lib/Users/user/login.dart
@@ -1,71 +1,136 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
-  const Login({Key? key}) : super(key: key);
-  void hello() { 
-    return;
-    }
+class Signup extends StatefulWidget {
+  const Signup({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return SignupState();
+  }
+}
+
+class SignupState extends State<Signup> {
+  late String _email;
+  late String _password;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Widget _buildEmail() {
+    return TextFormField(
+      decoration: const InputDecoration(
+        labelText: 'Email',
+        hintText: 'Email...',
+        border: OutlineInputBorder() ),
+      validator: ( value) {
+        if (value ==null) {
+          return 'Email is Required';
+        }
+
+        if (!RegExp(
+                r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+            .hasMatch(value)) {
+          return 'Please enter a valid email Address';
+        }
+
+        return null;
+      },
+      onSaved: ( value) {
+        _email = value!;
+      },
+    );
+  }
+
+  Widget _buildPassword() {
+    return TextFormField(
+      obscureText: true,
+      obscuringCharacter: "*",
+      decoration: const InputDecoration(
+        labelText: 'Password',
+        hintText: 'Password...',
+        border: OutlineInputBorder() ),
+      validator: ( value) {
+        if (value ==null) {
+          return 'Password is Required';
+        }
+
+        return null;
+      },
+      onSaved: ( value) {
+        _password = value!;
+      },
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFFFFFFF),
-      body: Column(
-          children:  [
-            const Padding(
-              padding: EdgeInsets.all(16.0),
-              child:   Image(
-                width: 200.0,
-                height: 150.0,
-                image: AssetImage("assets/Lama_Mahmoud_Eco_Sort.png"))
-                ),
-
-                //Email
-            const Padding(
-              padding: EdgeInsets.all(25.0),
-              child:TextField(
-                decoration: InputDecoration(  
-                  border: OutlineInputBorder()  ,
-                  labelText: 'Email',  
-                  hintText: 'Email...'  
-                ),  
-              )
-                ),
-
-                //password
-            const Padding(
-              padding: EdgeInsets.all(25.0),
-              child:TextField(
-                  obscureText: true,
-                  obscuringCharacter: "*",
-                  decoration: InputDecoration(
-                      hintText: "Enter password",
-                  border: OutlineInputBorder() 
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.all(24),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                 const Image(
+                  width: 200,
+                  height: 200,
+                  image: AssetImage("assets/Lama_Mahmoud_Eco_Sort.png")
                   ),
-                ),  
-              ),
-
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child:FractionallySizedBox(
-                widthFactor: 1.0,
-              child: TextButton(
-              child: const Padding(
-              padding: EdgeInsets.all(5.0),
-                child: Text("Login",
-                style: TextStyle(color: Colors.white, fontSize: 25,),),
+                _buildEmail(),
+                const SizedBox(height:30),
+                _buildPassword(),
+                const SizedBox(height: 45),
                 
+                FractionallySizedBox(
+                  widthFactor: 1.0,
+                  child: TextButton(
+                  child: const Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: Text("Login",
+                  style: TextStyle(
+                    color: Colors.white, 
+                    fontSize: 25,),),
+                  ),
+                  style: TextButton.styleFrom(
+                    backgroundColor: const Color(0xFF5BB259),
+                    primary: const Color(0xFFFEFEFE),
+                  ),onPressed: () {
+                    if (!_formKey.currentState!.validate()) {
+                      return;
+                    }
+
+                    _formKey.currentState!.save();
+
+                    print(_email);
+                    print(_password);
+
+                    //Send to API
+                  },
+                )
                 ),
-                // ignore: avoid_print
-                onPressed: ()=>print("hello world"),
-                style: TextButton.styleFrom(
-                  backgroundColor: const Color(0xFF5BB259),
-                  primary: const Color(0xFFFEFEFE),
-                )
-                )
-              )
-              )
-          ],
+                const SizedBox(height:10),
+                 Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text("Don't have an account?"),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        primary: const Color(0xFF5BB259),
+                      ),
+                      onPressed: ()=>{}, 
+                    child: const Text("Create One")
+                    )
+                  ])
+              ],
+            ),
+          ),
         ),
+      ),
     );
   }
 }
