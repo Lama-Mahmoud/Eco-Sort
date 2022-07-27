30d3e537d3bc6e536ef1a74c08a597bab89d7589
lama mahmoud <lamamahmoud213@gmail.com>
2022-07-28 01:12:53 +0300

    Frontend: Creating signup controller to lik to signup api

---
+++ frontend/lib/Users/user/signup_controller.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+
+class User{
+  late String fname, lname,email,password,city,region,role;
+
+  User(this.fname,this.lname,this.email,this.password,this.city,this.region,this.role);
+ 
+  void add () async{
+    var body= jsonEncode({"f_name":fname,
+    "l_name":lname,
+    "email":email,
+    "password":password,
+    "city":city,
+    "region":region,
+    "role":role});
+    var response= await http.post(Uri.http("127.0.0.1:3000", "api/user/auth/signup"),
+      headers: {"Content-Type": "application/json"},
+     body: body);
+    var data= response.headers;
+    print(email);
+    print(password);
+    int state= response.statusCode;
+    print(data);
+    print(state);
+  }
+
+}
