1246607869871c8d454a461238b7d7bf247a5cb8
lama mahmoud <lamamahmoud213@gmail.com>
2022-07-28 15:09:26 +0300

    Frontend: connecting to the signup api

--- frontend/lib/Users/user/role.dart
+++ frontend/lib/Users/user/role.dart
@@ -1,6 +1,8 @@
-
+// ignore_for_file: unnecessary_const, avoid_print, file_names
 
 import 'package:flutter/material.dart';
+import 'package:frontend/Users/user/signup_controller.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Role extends StatefulWidget {
   const Role({Key? key}) : super(key: key);
@@ -12,7 +14,7 @@ class Role extends StatefulWidget {
 }
 
 
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
 class RoleState extends State<Role> {
 @override
@@ -26,11 +28,102 @@ class RoleState extends State<Role> {
             key: _formKey,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: const <Widget>[
-                SizedBox(height:20),
-                Text("Are YOu?",
+              children:  <Widget>[
+                const SizedBox(height:70),
+                const Text("Are You?",
                 style: TextStyle(
                     color: Color(0xFF999999), 
                     fontSize: 25,)
-                ),])))));
-  }}
+                ),
+
+                
+                Padding(
+                  padding: const EdgeInsets.all(9.0),
+                  child:FractionallySizedBox(
+                  widthFactor: 1,
+                  child: TextButton(
+                  child: const Padding(
+                    padding: EdgeInsets.all(12.0),
+                      child: Text("Farmer",
+                        style: TextStyle(
+                          color: Colors.white, 
+                          fontSize: 20,
+                        ),),
+                        ),
+                        style: TextButton.styleFrom(
+                          backgroundColor: const Color(0xFF5BB259),
+                          primary: const Color(0xFFFEFEFE),
+                        ),
+                  onPressed: () async {
+                    final prefs =  await SharedPreferences.getInstance();
+                    String? email=prefs.getString('email');
+                    String? lName=prefs.getString('l_name');
+                    String? fName=prefs.getString('f_name');
+                    String? city=prefs.getString('city');
+                    String? region=prefs.getString('region');
+                    String? password=prefs.getString('password');
+                    
+                    User user=User(
+                      fName.toString(),
+                      lName.toString(),
+                      email.toString(),
+                      password.toString(),
+                      city.toString(),
+                      region.toString(),
+                      "Recycler");
+                      user.add();
+                  },
+                ),
+                  )
+                  ),
+                  
+
+                
+                Padding(
+                  padding: const EdgeInsets.all(9.0),
+                  child:FractionallySizedBox(
+                  widthFactor: 1,
+                  child: TextButton(
+                  child: const Padding(
+                    padding: EdgeInsets.all(12.0),
+                      child: Text("Recycler",
+                        style: TextStyle(
+                          color: Colors.white, 
+                          fontSize: 20,
+                        ),),
+                        ),
+                        style: TextButton.styleFrom(
+                          backgroundColor: const Color(0xFF5BB259),
+                          primary: const Color(0xFFFEFEFE),
+                        ),
+                  onPressed: () async {
+                    final prefs =  await SharedPreferences.getInstance();
+                    String? email=prefs.getString('email');
+                    String? lName=prefs.getString('l_nmae');
+                    String? fName=prefs.getString('f_name');
+                    String? city=prefs.getString('city');
+                    String? region=prefs.getString('region');
+                    String? password=prefs.getString('password');
+                    
+                    User user=User(
+                      lName.toString(),
+                      fName.toString(),
+                      email.toString(),
+                      password.toString(),
+                      city.toString(),
+                      region.toString(),
+                      "Recycler");
+                      user.add();
+                  },
+                ),
+                  )
+                  )
+
+              ]
+            )
+          )
+        )
+      )
+    );
+  }
+}
