73b0763452273dbd21f2047a94690fddc2cf2156
lama mahmoud <lamamahmoud213@gmail.com>
2022-07-28 16:33:14 +0300

    Frontend: Validating role on app opening

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:frontend/Users/user/login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 
 void main(){
@@ -12,13 +13,38 @@ void main(){
 }
 
 class MainPage extends StatelessWidget {
+
+  void next (BuildContext context)async{
+    
+    final prefs = await SharedPreferences.getInstance();
+    String? token =  prefs.getString("authToken");
+    if(token==null){
+      Timer(const Duration(seconds:2),(){
+         Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Login()));
+         });
+    }
+    else {
+      String? role =  prefs.getString("role");
+      if(role=="Farmer"){
+        
+      }
+      if(role=="Admin"){
+        
+      }
+      if(role=="Recycler"){
+        
+      }
+    }
+
+  }
+
   const MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-      Timer(const Duration(seconds:2),(){
-         Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Login()));
-         });  
+
+    next(context);
+        
     return const Scaffold(
       backgroundColor:Color(0xFF5BB259),
       body:Center(
