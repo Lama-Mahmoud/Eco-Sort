2a0e63f2e9df99c99a07b5d685789b4b4bd01ca4
lama mahmoud <lamamahmoud213@gmail.com>
2022-08-02 20:25:53 +0300

    Frontend: Major changes in transition and acessing shared prefs

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: avoid_print
+
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+//import 'package:frontend/Users/recycler/recycler.dart';
 import 'package:frontend/Users/user/login.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -14,36 +17,19 @@ void main(){
 
 class MainPage extends StatelessWidget {
 
-  void next (BuildContext context)async{
-    
-    final prefs = await SharedPreferences.getInstance();
-    String? token =  prefs.getString("authToken");
-    if(token==null){
-      Timer(const Duration(seconds:2),(){
-         Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Login()));
-         });
-    }
-    else {
-      String? role =  prefs.getString("role");
-      if(role=="Farmer"){
-        
-      }
-      if(role=="Admin"){
-        
-      }
-      if(role=="Recycler"){
-        
-      }
-    }
-
-  }
-
   const MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
 
-    //next(context);
+    Timer(const Duration(seconds:2),()async{
+        final prefs = await SharedPreferences.getInstance();
+        String? token =  prefs.getString("authToken");
+        print(token);
+        if(token==null){
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Login()));
+          }
+         });
         
     return const Scaffold(
       backgroundColor:Color(0xFF5BB259),
